a1e0fe9af44c61cd71aec8eb39e4bb571003730f
Beamlnwza <Supawit04591@pcccr.ac.th>
2023-03-20 11:52:10 +0700

    second mate

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <RecoilRoot>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<App />} />
+                    <Route path="/" element={<Works />} />
                     <Route path="/works" element={<Works />} />
                     <Route path="/teacher" element={<Teacher />} />
                     <Route path="/teacher/register" element={<Register />} />
